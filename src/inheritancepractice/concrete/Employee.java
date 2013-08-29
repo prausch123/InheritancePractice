ab3fc280835aacf391e0b3c5300a46bc05aa9d3d
Peter <Peter@Peter-PC>
2013-08-29 12:45:38 -0500

    First commit

---
+++ src/inheritancepractice/concrete/Employee.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancepractice.concrete;
+
+/**
+ *
+ * @author Peter
+ */
+public class Employee {
+    String name;
+    String position;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
